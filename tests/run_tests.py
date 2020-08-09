ab809aad50837dd032667b49295ab338d28405e7
Arunav Shandilya <Arunav>
2020-08-09 21:50:49 +0530

    test for deepl translator

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,5 +1,5 @@
 import sys
-import test_sentence, test_project, test_stt, test_translate, test_tts
+import test_sentence, test_project, test_stt, test_translate, test_tts, test_deepl_translate
 
 # runs all the tests we have, should exit with 0
 if __name__ == "__main__":
@@ -9,4 +9,5 @@ if __name__ == "__main__":
     err += test_stt.main()
     err += test_translate.main()
     err += test_tts.main()
+    err += test_deepl_translate.main()
     sys.exit(err)
